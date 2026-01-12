a768098f97f65caa61890f351edeafbb412eaa25
shtirlizc <shtirlizc@gmail.com>
2026-01-12 12:56:45 +0500

    downgrade prisma to 6 version, add district migration

---
+++ prisma/migrations/20260112075540_add_district_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "District" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "District_pkey" PRIMARY KEY ("id")
+);
