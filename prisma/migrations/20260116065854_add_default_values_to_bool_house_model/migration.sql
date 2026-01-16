62454cbec6ee5fda7b5dc3d704a8997e61225c98
shtirlizc <shtirlizc@gmail.com>
2026-01-16 11:59:41 +0500

    add_default_values_to_bool_house_model migrations

---
+++ prisma/migrations/20260116065854_add_default_values_to_bool_house_model/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "House" ALTER COLUMN "asphaltToHouse" SET DEFAULT false,
+ALTER COLUMN "closedComplex" SET DEFAULT false,
+ALTER COLUMN "separatedKitchenLiving" SET DEFAULT false,
+ALTER COLUMN "hasMinimumDownPayment" SET DEFAULT true,
+ALTER COLUMN "isActive" SET DEFAULT true;
